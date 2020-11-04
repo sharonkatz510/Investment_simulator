3a52a49123aef91dfd770cbe6c76c9f0a39a051b
sharonkatz510 <sharonkats510@gmail.com>
2020-11-04 10:25:43 +0200

    DashApp is running smooth Added documentation

--- dashApp.py
+++ dashApp.py
@@ -5,7 +5,7 @@ import dash_daq as daq
 import dash_html_components as html
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 from portfolio import read_portfolio_from_pickle, Portfolio
 
@@ -23,6 +23,10 @@ fig1.update_layout(yaxis_tickformat='%')
 
 
 def slider(i):
+    """
+    :param i: index of slider for identification
+    :return: Slider object with label
+    """
     return html.Div([html.Label(ptf.summary['name'][i]),
                      daq.Slider(id='stkWeight{0}'.format(i),
                                 min=0, max=100, value=100, step=10, size=200,
@@ -41,54 +45,61 @@ def slider(i):
 app.layout = html.Div([
     html.Div([dcc.Input(id='ticker-name', type='text', placeholder='Insert ticker symbol'),
               html.Button('Add', id='add-button'), html.Button('Remove', id='remove-button')]),
-    dcc.Graph(id='all-ticker-graph', figure=fig1),
-    html.Div(id='sliders', children=[slider(i) for i in range(len(w.index))]),
-    dcc.Graph(id='combined-graph'),
-    dcc.Graph(id='pie-plots')
+    html.Div(id='all-assets', children=[dcc.Graph(id='all-ticker-graph', figure=fig1),
+                                        html.Div(id='sliders', children=[slider(i) for i in range(len(w.index))])]),
+    html.Div(id='combined-graph'),
 ])
 
 
 #   App callbacks
-@app.callback(Output('all-ticker-graph', 'figure'),
-              [Input('add-button', 'n_clicks'), Input('ticker-name', 'value')])
-def update_portfolio(_, tick):
-    ptf.add(tick)
-    scaled = ptf.get_scaled_prices()
+
+@app.callback(Output('all-assets', 'children'), [Input('add-button', 'n_clicks'), Input('remove-button', 'n_clicks')],
+              [State('ticker-name', 'value')])
+def update_assets(_1, _2, tick):
+    """
+    Update assets when adding or removing an asset
+    :param _1: add-button n_clicks as unused input
+    :param _2: remove-button n_clicks as unused input
+    :param tick: ticker-name in input box
+    :return: update all-assets figure and sliders
+    """
+    global ptf, scaled
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    if 'add-button' in changed_id:
+        scaled = ptf.add(tick).get_scaled_prices()
+    elif 'remove-button' in changed_id:
+        scaled = ptf.remove(tick).get_scaled_prices()
     figure = px.line(scaled, title='Ticker revenue comparison')
     figure.update_layout(yaxis_tickformat='%')
-    return figure
+    div = [dcc.Graph(id='all-ticker-graph', figure=figure),
+           html.Div(id='sliders', children=[slider(i) for i in range(len(ptf.finance.columns))])]
+    return div
 
 
-@app.callback(Output('sliders', 'children'),
-              [Input('add-button', 'n_clicks'), Input('ticker-name', 'value')])
-def update_sliders(_, tick):
-    ptf.add(tick)
-    w = ptf.summary['weight']
-    return [slider(i) for i in range(len(w.index))]
+@app.callback(Output('ticker-name', 'value'), [Input('add-button', 'n_clicks'), Input('remove-button', 'n_clicks')])
+def clear_input(_1, _2):
+    return ''
 
 
-@app.callback(Output('combined-graph', 'figure'),
+@app.callback(Output('combined-graph', 'children'),
               [Input(component_id='stkWeight{}'.format(i), component_property='value') for i in range(len(w.index))])
-def update_combined_figure(*ws):
+def update_combined_figures(*ws):
+    """
+    Update portfolio exposure and revenue according to sliders
+    :param ws: list of slider values
+    :return: combined revenue figure, currency exposure pie chart, sector exposure pie chart
+    """
     a = ptf.update(weights=list(ws))
-    figure = px.line(a.get_combined_worth(), title='Combined revenue')
-    figure.update_layout(yaxis_tickformat='%', showlegend=False)
-    return figure
-
-
-@app.callback(Output('pie-plots', 'figure'),
-              [Input(component_id='stkWeight{}'.format(i), component_property='value') for i in range(len(w.index))])
-def update_pie_charts(*ws):
-    a = ptf.update(weights=list(ws))
+    figure1 = px.line(a.get_combined_worth(), title='Combined revenue')
+    figure1.update_layout(yaxis_tickformat='%', showlegend=False)
     sector_split = a.get_sector_split()
-    figure = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]])
-    figure.add_trace(go.Pie(labels=a.currencySplit.index, values=a.currencySplit['weight'],
-                            name='Currency', title='Currency exposure'),
-                     row=1, col=1)
-    figure.add_trace(go.Pie(labels=sector_split.index, values=sector_split.to_list(),
-                            name='Sector', title='Sector exposure'),
-                     row=1, col=2)
-    return figure
+    figure2 = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]])
+    figure2.add_trace(go.Pie(labels=a.currencySplit.index, values=a.currencySplit['weight'],
+                             name='Currency', title='Currency exposure'), row=1, col=1)
+    figure2.add_trace(go.Pie(labels=sector_split.index, values=sector_split.to_list(),
+                             name='Sector', title='Sector exposure'), row=1, col=2)
+    div = html.Div([dcc.Graph(figure=figure1), dcc.Graph(figure=figure2)])
+    return div
 
 
 app.run_server(debug=False)
--- portfolio.py
+++ portfolio.py
@@ -32,11 +32,18 @@ class Portfolio:    # TODO: add ability to change resolution
             pickle.dump(self, fid)  # number of assets to load
 
     def update(self, tickers=None, period=None, weights=None):
+        """
+        Update portfolio when passing new tickers, period or weights
+        :param tickers: (str or list) assets symbols
+        :param period: (int) number of years
+        :param weights: (list) weights for assets in portfolio
+        :return: updated portfolio
+        """
         if tickers or period:
             self.finance = get_all_ticker_close(tickers, period or self.period)
         if tickers:
             self.summary = get_all_ticker_info(tickers)
-        if weights:
+        if weights and (len(weights) == self.summary.shape[0]):
             self.summary['weight'] = np.array(weights)/np.array(weights).sum()
         elif 'weight' not in self.summary.columns:
             self.summary['weight'] = (1/self.summary.shape[0])*np.ones(self.summary.shape[0])
@@ -44,29 +51,54 @@ class Portfolio:    # TODO: add ability to change resolution
         return self
 
     def get_scaled_prices(self):
+        """
+        Scale prices of assets to start from 100%
+        :return: scaled prices matrix of shape: time x n_assets
+        """
         prices_scaled = self.finance.apply(lambda x: x / x[x.first_valid_index()], axis=0).fillna(method='ffill')
         return prices_scaled.rename(columns=self.summary['name'].to_dict())
 
     def get_combined_worth(self):
+        """
+        Calculate portfolio revenue w/ respect to each asset revenue and weight
+        :return: combined worth (as % of start value) vector
+        """
         prices_scaled = self.finance.apply(lambda x: x / x[x.first_valid_index()], axis=0).fillna(method='ffill')
         w = self.summary['weight']
         w.index = prices_scaled.columns
         return (prices_scaled * w.transpose()).sum(axis=1).to_frame(name='Combined value')
 
     def remove(self, tick):
-        self.summary.drop(tick, axis=0, inplace=True)
-        self.finance.drop(tick, axis=1, inplace=True)
-        self.update()
+        """
+        Remove asset from portfolio
+        :param tick: (str) symbol
+        :return: updated portfolio
+        """
+        if tick in self.finance.columns:
+            self.summary.drop(tick, axis=0, inplace=True)
+            self.finance.drop(tick, axis=1, inplace=True)
+            self.update()
+        return self
 
-    def add(self, tick):    # TODO: fix
+    def add(self, tick):
+        """
+        Add asset to portfolio
+        :param tick: (str) symbol
+        :return: updated portfolio
+        """
         dat = get_all_ticker_close(tick, self.period).to_frame().rename(columns={'Adj Close': tick})
         self.finance = self.finance.join(dat)
         info = get_all_ticker_info(tick)
         self.summary.drop('weight', axis=1, inplace=True)
         self.summary = self.summary.append(info)
         self.update()
+        return self
 
     def get_sector_split(self):
+        """
+        Get sector split of portfolio w/ respect to assets weights
+        :return: pandas.Series w/ indices=sectors and values= % of portfolio
+        """
         query = yq.Ticker(self.summary['ticker'].to_list())
         data = pd.DataFrame.from_dict(query.fund_sector_weightings)
         mult = np.array(data) * np.array(self.summary['weight'].transpose())
@@ -74,6 +106,12 @@ class Portfolio:    # TODO: add ability to change resolution
 
 
 def get_all_ticker_close(tickers, period):
+    """
+    Extract adj. close data for all tickers in the time period
+    :param tickers: (str or list of strings) assets symbols
+    :param period: (int) time period in years
+    :return: pandas.DataFrame of adj. close prices where indices=Dates columns= assets symbols
+    """
     start_date = date.today().replace(year=date.today().year - period)
     end_date = date.today()
     query = pdr.YahooDailyReader(tickers, start=start_date, end=end_date, interval='m')
@@ -82,15 +120,24 @@ def get_all_ticker_close(tickers, period):
 
 
 def get_all_ticker_info(tickers):
+    """
+    Extract fundamental information about assets
+    :param tickers: (str or list of strings) assets symbols
+    :return: panda.DataFrame of assets fund. info where indices=symbols, columns={'ticker','exchange','name','currency'}
+    """
     query = yq.Ticker(tickers)
     currency = pd.DataFrame.from_dict(query.summary_detail).loc['currency', :]
     data = pd.DataFrame.from_dict(query.quote_type).loc[['symbol', 'exchange', 'shortName'], :]
     data = data.append(currency)
-    data.rename(indices={'shortName': 'name', 'symbol': 'ticker'}, inplace=True)
+    data.rename(index={'shortName': 'name', 'symbol': 'ticker'}, inplace=True)
     return data.transpose()
 
 
 def read_portfolio_from_pickle(path):
+    """
+    :param path: (str) path to pickle file
+    :return: Portfolio object
+    """
     with open(path, 'rb') as fid:
         portfolio = pickle.load(fid)
     return portfolio
