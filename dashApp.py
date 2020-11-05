4dcf7be1d70b1ea344239d646ca477abacc5ebd1
sharonkatz510 <sharonkats510@gmail.com>
2020-11-05 16:54:49 +0200

    Fixed updating assets problem Better data handling Added saving portfolio option

--- dashApp.py
+++ dashApp.py
@@ -4,15 +4,13 @@
 import dash
 import plotly.express as px
 import dash_core_components as dcc
-import dash_daq as daq
 import dash_html_components as html
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, ALL
+from portfolio import read_portfolio_from_pickle, Portfolio, pd
 
-from portfolio import read_portfolio_from_pickle, Portfolio
-
-#   get data
+#   get default data
 
 ptf = read_portfolio_from_pickle('portfolio.pkl')
 w = ptf.summary['weight']
@@ -20,93 +18,168 @@ scaled = ptf.get_scaled_prices()
 
 #   Create app
 app = dash.Dash()
-#   create static figure - multi-line plot
-fig1 = px.line(scaled, title='Ticker revenue comparison')
-fig1.update_layout(yaxis_tickformat='%')
 
 
-def slider(i):
+def slider(i: int, ptf: Portfolio):
     """
-    :param i: index of slider for identification
-    :return: Slider object with label
+    Slider object constructor
+    :param i: index of slider (corresponding to asset #i)
+    :param ptf: Portfolio
+    :return: slider w/ range [0-100], upper title= asset name, lower title= asset symbol
     """
     return html.Div([html.Label(ptf.summary['name'][i]),
-                     daq.Slider(id='stkWeight{0}'.format(i),
-                                min=0, max=100, value=100, step=10, size=200,
-                                handleLabel={"showCurrentValue": True, 'label': '%'},
-                                labelPosition='bottom',
+                     dcc.Slider(id={'type': 'slider', 'index': i},
+                                min=0, max=100, value=100, step=10,
                                 marks={0: {'label': '0%'},
-                                       100: {'label': '100%'}})],
+                                       100: {'label': '100%'}}),
+                     html.Label(ptf.summary.index[i])],
                     style={'display': 'inline-block',
                            'margin-right': 50,
                            'margin-left': 50,
-                           'margin-top': 50,
-                           'margin-bottom': 50})
+                           'margin-top': 20,
+                           'margin-bottom': 20,
+                           'width': 300,
+                           'textAlign': 'center'})
 
+
+#   create default figs
+fig1 = px.line(scaled, title='Ticker revenue comparison')
+fig1.update_layout(yaxis_tickformat='%')
+fig2 = px.line(ptf.get_combined_worth(), title='Combined revenue')
+fig2.update_layout(yaxis_tickformat='%', showlegend=False)
+sector_split = ptf.get_sector_split()
+currency_split = ptf.get_currency_split()
+fig3 = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]])
+fig3.add_trace(go.Pie(labels=currency_split.index, values=currency_split['weight'],
+                      name='Currency', title='Currency exposure'), row=1, col=1)
+fig3.add_trace(go.Pie(labels=sector_split.index, values=sector_split.to_list(),
+                      name='Sector', title='Sector exposure'), row=1, col=2)
+div2 = html.Div([dcc.Graph(figure=fig2), dcc.Graph(figure=fig3)])
 
 #   Configure HTML layout
 app.layout = html.Div([
     html.Div([dcc.Input(id='ticker-name', type='text', placeholder='Insert ticker symbol'),
               html.Button('Add', id='add-button'), html.Button('Remove', id='remove-button')]),
-    html.Div(id='all-assets', children=[dcc.Graph(id='all-ticker-graph', figure=fig1),
-                                        html.Div(id='sliders', children=[slider(i) for i in range(len(w.index))])]),
-    html.Div(id='combined-graph'),
+    html.Div(id='all-assets',
+             children=[dcc.Graph(id='all-ticker-graph', figure=fig1),
+                       html.Div(id='sliders', children=[slider(i, ptf) for i in range(len(w.index))])]),
+    html.Button('Update weights', id='update-button'),
+    html.Div(id='combined-graph', children=div2),
+    html.Div([html.Button('Save portfolio', id='save-button'), html.Div(id='save-confirmation')],
+             style={'display': 'inline-block'}),
+    # Data sharing within the app
+    html.Div(id='portfolio-data', children=[
+        html.Div(id='portfolio-finance', children=ptf.finance.to_json(), style={'display': 'none'}),
+        html.Div(id='portfolio-fund', children=ptf.summary.to_json(), style={'display': 'none'}),
+        html.Div(id='portfolio-period', children=ptf.period, style={'display': 'none'}),
+        html.Div(id='last_trigger', children=None, style={'display': 'none'})])
 ])
 
 
 #   App callbacks
-
-@app.callback(Output('all-assets', 'children'), [Input('add-button', 'n_clicks'), Input('remove-button', 'n_clicks')],
-              [State('ticker-name', 'value')])
-def update_assets(_1, _2, tick):
+@app.callback(Output('portfolio-data', 'children'),
+              [Input('add-button', 'n_clicks'),
+               Input('remove-button', 'n_clicks'),
+               Input('update-button', 'n_clicks')],
+              [State('portfolio-finance', 'children'),
+               State('portfolio-fund', 'children'),
+               State('portfolio-period', 'children'),
+               State('ticker-name', 'value'),
+               State({'type': 'slider', 'index': ALL}, 'value')])
+def update_assets(_1, _2, _3, finance, fund, period, tick, *ws):
     """
-    Update assets when adding or removing an asset
-    :param _1: add-button n_clicks as unused input
-    :param _2: remove-button n_clicks as unused input
-    :param tick: ticker-name in input box
-    :return: update all-assets figure and sliders
+    Update portfolio when adding an asset, removing an asset or updating the weights
     """
-    global ptf, scaled
+    finance = pd.read_json(finance); summary = pd.read_json(fund)
+    ptf = Portfolio(finance=finance, summary=summary, period=period)
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-    if 'add-button' in changed_id:
-        scaled = ptf.add(tick).get_scaled_prices()
-    elif 'remove-button' in changed_id:
-        scaled = ptf.remove(tick).get_scaled_prices()
-    figure = px.line(scaled, title='Ticker revenue comparison')
-    figure.update_layout(yaxis_tickformat='%')
-    div = [dcc.Graph(id='all-ticker-graph', figure=figure),
-           html.Div(id='sliders', children=[slider(i) for i in range(len(ptf.finance.columns))])]
+    trigger = None
+    if 'add' in changed_id:
+        ptf.add(tick)
+        trigger = 'add'
+    elif 'remove' in changed_id:
+        ptf.remove(tick)
+        trigger = 'remove'
+    elif 'update' in changed_id:
+        ptf.update(weights=ws[0])
+        trigger = 'update'
+    div = [
+        html.Div(id='portfolio-finance', children=ptf.finance.to_json(), style={'display': 'none'}),
+        html.Div(id='portfolio-fund', children=ptf.summary.to_json(), style={'display': 'none'}),
+        html.Div(id='portfolio-period', children=ptf.period, style={'display': 'none'}),
+        html.Div(id='last_trigger', children=trigger, style={'display': 'none'})]
+    return div
+
+
+@app.callback(Output('all-assets', 'children'), [Input('last_trigger', 'children')],
+              [State('portfolio-finance', 'children'),
+               State('portfolio-fund', 'children'),
+               State('portfolio-period', 'children'),
+               State('all-assets', 'children')])
+def update_multi_asset_objects(trigger, finance, fund, period, div):
+    """
+    Update sliders and asset revenue comparison graph when asset list changes
+    """
+    finance = pd.read_json(finance); summary = pd.read_json(fund)
+    ptf = Portfolio(finance=finance, summary=summary, period=period)
+    if trigger == 'add' or trigger == 'remove':
+        scaled = ptf.get_scaled_prices()
+        figure = px.line(scaled, title='Ticker revenue comparison')
+        figure.update_layout(yaxis_tickformat='%')
+        div = [dcc.Graph(id='all-ticker-graph', figure=figure),
+               html.Div(id='sliders', children=[slider(i, ptf) for i in range(len(finance.columns))])]
+    return div
+
+
+@app.callback(Output('combined-graph', 'children'), [Input('last_trigger', 'children')],
+              [State('portfolio-finance', 'children'),
+               State('portfolio-fund', 'children'),
+               State('portfolio-period', 'children'),
+               State('combined-graph', 'children')])
+def update_combined_figures(trigger, finance, fund, period, div):
+    """
+    Update combined worth graph and exposure pie charts
+    """
+    finance = pd.read_json(finance); summary = pd.read_json(fund)
+    ptf = Portfolio(finance=finance, summary=summary, period=period)
+    if trigger is not None:
+        figure1 = px.line(ptf.get_combined_worth(), title='Combined revenue')
+        figure1.update_layout(yaxis_tickformat='%', showlegend=False)
+        sector_split = ptf.get_sector_split()
+        currency_split = ptf.get_currency_split()
+        figure2 = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]])
+        figure2.add_trace(go.Pie(labels=currency_split.index, values=currency_split['weight'],
+                                 name='Currency', title='Currency exposure'), row=1, col=1)
+        figure2.add_trace(go.Pie(labels=sector_split.index, values=sector_split.to_list(),
+                                 name='Sector', title='Sector exposure'), row=1, col=2)
+        div = html.Div([dcc.Graph(figure=figure1), dcc.Graph(figure=figure2)])
     return div
 
 
 @app.callback(Output('ticker-name', 'value'), [Input('add-button', 'n_clicks'), Input('remove-button', 'n_clicks')])
 def clear_input(_1, _2):
+    """
+    Clear input box when adding or removing an asset
+    """
     return ''
 
 
-@app.callback(Output('combined-graph', 'children'),
-              [Input(component_id='stkWeight{}'.format(i), component_property='value') for i in range(len(w.index))])
-def update_combined_figures(*ws):
+@app.callback(Output('save-confirmation', 'children'), [Input('save-button', 'n_clicks')],
+              [State('portfolio-finance', 'children'),
+               State('portfolio-fund', 'children'),
+               State('portfolio-period', 'children')], prevent_initial_call=True)
+def save_content(_, finance, fund, period):
     """
-    Update portfolio exposure and revenue according to sliders
-    :param ws: list of slider values
-    :return: combined revenue figure, currency exposure pie chart, sector exposure pie chart
+    Save portfolio to default portfolio.pkl file
     """
-    a = ptf.update(weights=list(ws))
-    figure1 = px.line(a.get_combined_worth(), title='Combined revenue')
-    figure1.update_layout(yaxis_tickformat='%', showlegend=False)
-    sector_split = a.get_sector_split()
-    figure2 = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]])
-    figure2.add_trace(go.Pie(labels=a.currencySplit.index, values=a.currencySplit['weight'],
-                             name='Currency', title='Currency exposure'), row=1, col=1)
-    figure2.add_trace(go.Pie(labels=sector_split.index, values=sector_split.to_list(),
-                             name='Sector', title='Sector exposure'), row=1, col=2)
-    div = html.Div([dcc.Graph(figure=figure1), dcc.Graph(figure=figure2)])
-    return div
+    finance = pd.read_json(finance); summary = pd.read_json(fund)
+    ptf = Portfolio(finance=finance, summary=summary, period=period)
+    ptf.save_to_pickle('portfolio.pkl')
+    return 'Portfolio successfully saved'
 
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
 
 __author__ = "Sharon Katz"
 __email__ = 'sharonkats510@gmail.com'
--- portfolio.py
+++ portfolio.py
@@ -1,43 +1,42 @@
-import pandas as pd
-import pandas_datareader.yahoo.daily as pdr
-import yahooquery as yq
 from datetime import date
 import numpy as np
+import pandas as pd
 import pickle
+import pandas_datareader.yahoo.daily as pdr
+import yahooquery as yq
 #
 
 
 class Portfolio:
     """Bundle of assets with different weights"""
-    def __init__(self, tickers, period=10, weights: list = None, finance=None):
-        """
-        :param tickers: list of tickers with somw general info
-        :param period: time period in years
-        :param weights: list of weights for averaging
-        """
+    def __init__(self, tickers: list = None, period: int = 10, weights: list = None,
+                 finance: pd.DataFrame = None, summary: pd.DataFrame = None):
         self.period = period
-        self.finance = finance or get_all_ticker_close(tickers, period)
-        self.summary = get_all_ticker_info(tickers)
-        self.summary['weight'] = np.array(weights)/np.array(weights).sum() if weights else \
-            (1/self.summary.shape[0])*np.ones(self.summary.shape[0])
-        # extract currency exposure
-        self.currencySplit = get_weighted_count(self.summary, 'currency')
+        if finance is not None:
+            self.finance = finance
+            self.summary = summary
+        elif tickers is not None:
+            self.finance = get_all_ticker_close(tickers, period)
+            self.summary = get_all_ticker_info(tickers)
+        if 'weight' not in self.summary.columns:
+            self.summary['weight'] = np.array(weights)/np.array(weights).sum() if weights else \
+                (1/self.summary.shape[0])*np.ones(self.summary.shape[0])
 
     def save_to_pickle(self, path: str):
         """
-        Save portfolio to pickle file, compatible with load_portfolio_from_pickle function
-        :param path: (string) path to new pickle file
+        Read portfolio from pickle file
+        :param path: relative path to pickle file
         """
         with open(path, 'wb') as fid:
             pickle.dump(self, fid)  # number of assets to load
 
-    def update(self, tickers=None, period=None, weights=None):
+    def update(self, tickers: list = None, period: int = None, weights: list = None):
         """
-        Update portfolio when passing new tickers, period or weights
-        :param tickers: (str or list) assets symbols
-        :param period: (int) number of years
-        :param weights: (list) weights for assets in portfolio
-        :return: updated portfolio
+        Update portfolio w/ new tickers, period or weights
+        :param tickers: list of strings with asset symbols
+        :param period: time period in years
+        :param weights: list of weights corresponding to asset symbols
+        :return: updated Portfolio
         """
         if tickers or period:
             self.finance = get_all_ticker_close(tickers, period or self.period)
@@ -47,70 +46,72 @@ class Portfolio:
             self.summary['weight'] = np.array(weights)/np.array(weights).sum()
         elif 'weight' not in self.summary.columns:
             self.summary['weight'] = (1/self.summary.shape[0])*np.ones(self.summary.shape[0])
-        self.currencySplit = get_weighted_count(self.summary, 'currency')
         return self
 
-    def get_scaled_prices(self):
+    def get_scaled_prices(self) -> pd.DataFrame:
         """
-        Scale prices of assets to start from 100%
-        :return: scaled prices matrix of shape: time x n_assets
+        Scale asset prices to % of initial value
+        :return: DataFrame of asset adjusted-scaled prices over the time period
         """
         prices_scaled = self.finance.apply(lambda x: x / x[x.first_valid_index()], axis=0).fillna(method='ffill')
         return prices_scaled.rename(columns=self.summary['name'].to_dict())
 
-    def get_combined_worth(self):
+    def get_combined_worth(self) -> pd.DataFrame:
         """
-        Calculate portfolio revenue w/ respect to each asset revenue and weight
-        :return: combined worth (as % of start value) vector
+        Extract Portfolio revenue relative to start point w/ respect to asset weights
+        :return: Time series with worth as % of initial worth
         """
         prices_scaled = self.finance.apply(lambda x: x / x[x.first_valid_index()], axis=0).fillna(method='ffill')
         w = self.summary['weight']
         w.index = prices_scaled.columns
         return (prices_scaled * w.transpose()).sum(axis=1).to_frame(name='Combined value')
 
-    def remove(self, tick):
+    def remove(self, tick: str):
         """
         Remove asset from portfolio
-        :param tick: (str) symbol
-        :return: updated portfolio
+        :param tick: string object of asset symbol
+        :return: updated Portfolio
         """
         if tick in self.finance.columns:
             self.summary.drop(tick, axis=0, inplace=True)
             self.finance.drop(tick, axis=1, inplace=True)
-            self.update()
-        return self
+        return self.update()
 
-    def add(self, tick):
+    def add(self, tick: str):
         """
         Add asset to portfolio
-        :param tick: (str) symbol
-        :return: updated portfolio
+        :param tick: string object of asset symbol
+        :return: updated Portfolio
         """
         dat = get_all_ticker_close(tick, self.period).to_frame().rename(columns={'Adj Close': tick})
         self.finance = self.finance.join(dat)
         info = get_all_ticker_info(tick)
         self.summary.drop('weight', axis=1, inplace=True)
         self.summary = self.summary.append(info)
-        self.update()
-        return self
+        return self.update()
 
-    def get_sector_split(self):
+    def get_sector_split(self) -> pd.DataFrame:
         """
-        Get sector split of portfolio w/ respect to assets weights
-        :return: pandas.Series w/ indices=sectors and values= % of portfolio
+        Get sector distribution of portfolio w/ respect to weights
         """
         query = yq.Ticker(self.summary['ticker'].to_list())
         data = pd.DataFrame.from_dict(query.fund_sector_weightings)
         mult = np.array(data) * np.array(self.summary['weight'].transpose())
         return pd.Series(mult.sum(axis=1), index=data.index)
 
+    def get_currency_split(self) -> pd.DataFrame:
+        """
+        Get currency distribution of portfolio w/ respect to weights
+        """
+        return get_weighted_count(self.summary, 'currency')
 
-def get_all_ticker_close(tickers, period):
+
+def get_all_ticker_close(tickers: str or list, period: int) -> pd.DataFrame:
     """
-    Extract adj. close data for all tickers in the time period
-    :param tickers: (str or list of strings) assets symbols
-    :param period: (int) time period in years
-    :return: pandas.DataFrame of adj. close prices where indices=Dates columns= assets symbols
+    Extract adjusted closing prices for asset/s over a period of time
+    :param tickers: string or list of strings with asset symbols
+    :param period: Time period in years
+    :return: DataFrame with time series/es of asset adjusted closing prices
     """
     start_date = date.today().replace(year=date.today().year - period)
     end_date = date.today()
@@ -119,11 +120,11 @@ def get_all_ticker_close(tickers, period):
     return data['Adj Close']
 
 
-def get_all_ticker_info(tickers):
+def get_all_ticker_info(tickers: str or list) -> pd.DataFrame:
     """
     Extract fundamental information about assets
-    :param tickers: (str or list of strings) assets symbols
-    :return: panda.DataFrame of assets fund. info where indices=symbols, columns={'ticker','exchange','name','currency'}
+    :param tickers: string or list of strings with asset symbols
+    :return: DataFrame with the assets and the following characteristics: currency, ticker and name
     """
     query = yq.Ticker(tickers)
     currency = pd.DataFrame.from_dict(query.summary_detail).loc['currency', :]
@@ -133,9 +134,10 @@ def get_all_ticker_info(tickers):
     return data.transpose()
 
 
-def read_portfolio_from_pickle(path):
+def read_portfolio_from_pickle(path: str) -> Portfolio:
     """
-    :param path: (str) path to pickle file
+    Read portfolio from pickle file
+    :param path: local path to pickle file
     :return: Portfolio object
     """
     with open(path, 'rb') as fid:
@@ -143,10 +145,10 @@ def read_portfolio_from_pickle(path):
     return portfolio
 
 
-def get_weighted_count(df: pd.DataFrame, column: str):
+def get_weighted_count(df: pd.DataFrame, column: str) -> pd.DataFrame:
     """
     calculate distributions in data
-    :param df: (DataFrame) with a column with the specified name, and a 'weight' colummn
+    :param df: DataFrame with a column of the specified name, and a 'weight' colummn
     :param column: column name of the specific column in df
     :return: dictionary where keys = unique values in df[column] , and values = sum(<index weight>*<appeared or not>)
     """
